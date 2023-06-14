773d2661e3be356442abe23977c1aece5d6008ce
Mario Carballo Zama <afk@ellugar.co>
2023-06-14 08:53:31 -0600

    feat: add license to games page

--- views/games/games-notes/index.js
+++ views/games/games-notes/index.js
@@ -9,6 +9,7 @@ La información en esta página fue recopilada originalmente en su mayoría por 
 - Base de datos original en [Google Docs](https://docs.google.com/spreadsheets/d/1qZNjZOXthLsm_NQynQ2VOPgVUMK6hfAuLeTj1HG-bV0/edit#gid=1938942876)
 - [JSON de INDI·ES](https://github.com/indi-es/juegos/blob/main/data.json)
 
+Este trabajo esta licenciado con la [Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License](https://creativecommons.org/licenses/by-nc-sa/4.0/)
 `;
 
 function GamesNotes() {
